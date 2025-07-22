e53988a8f61f1cf81f2799ff3d8dd26c3dad46e2
Lívia <liviapnd@gmail.com>
2025-07-21 21:18:55 -0300

    fix(repository): use the task model instead repository

--- src/main/java/back/end/tasksapi/repository/TaskRepository.java
+++ src/main/java/back/end/tasksapi/repository/TaskRepository.java
@@ -1,6 +1,7 @@
 package back.end.tasksapi.repository;
 
+import back.end.tasksapi.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRepository extends JpaRepository<TaskRepository, String> {
+public interface TaskRepository extends JpaRepository<Task, String> {
 }
